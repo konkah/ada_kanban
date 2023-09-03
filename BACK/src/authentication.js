ee639dbb5f1d8b9f0063d242c7b63c0873f3bceb
Karlos Helton Braga <karlos.hbraga@gmail.com>
2023-09-03 20:55:27 +0100

    Add user authentication to backend api

---
+++ BACK/src/authentication.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken')
+
+const jwtSecret = process.env.JWT_SECRET;
+const apiUsername = process.env.API_USERNAME;
+const apiPassword = process.env.API_PASSWORD;
+
+exports.login = (request, response) => {
+    const { username, password } = request.body;
+
+    if (username === apiUsername && password === apiPassword) {
+        var token = jwt.sign({username}, jwtSecret, {'expiresIn': '10m'});
+        return response.json({'token': token});
+    }
+
+    return response.sendStatus(403);
+}
+
+exports.auth = (request, response, next) => {
+    if (request.path === '/login') {
+        return next(); // skip auth for login endpoint
+    }
+
+    const authHeader = request.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null)
+        return response.sendStatus(401);
+
+    return jwt.verify(
+        token,
+        jwtSecret,
+        
+        (err, _) => {
+            if (err)
+                return response.sendStatus(403);
+            return next();
+        }
+    
+    );
+}
--- BACK/src/index.js
+++ BACK/src/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const { auth, login } = require('./authentication')
+
 
 const app = express()
 app.use(express.json())
 
 app.get('/', (request, response) => response.json({status: 'online'}))
+
+app.use(auth)
+
+app.post('/login', login)
 
 app.listen(5000, () => console.log('Access http://localhost:5000'));
