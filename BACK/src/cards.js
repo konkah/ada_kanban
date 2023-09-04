ea5207d0631d0c1d0f2120a0138a3153769ac493
Karlos Helton Braga <karlos.hbraga@gmail.com>
2023-09-04 03:28:38 +0100

    Include update and delete cards to the project

--- BACK/src/cards.js
+++ BACK/src/cards.js
@@ -14,14 +14,17 @@ exports.getCards = async (request, response) => {
 }
 
 exports.postCards = async (request, response) => {
-    const { title, content } = request.body;
+    const { title, content, column } = request.body;
 
-    const card = { title, content, column: 'TODO' };
+    const card = { title, content, column };
+
+    if (!cardDataIsValid(card))
+        return response.sendStatus(400);
 
     const dbCard = await Card.create(card)
         .catch(
             (error) => {
-                console.error('Failed to create card: ', error);
+                console.error('Failed to create data: ', error);
                 response.sendStatus(500);
             }
         );
@@ -29,3 +32,121 @@ exports.postCards = async (request, response) => {
     return response.json(dbCard);
 }
 
+const cardDataIsValid = (card) => {
+    return card.title != null
+        && card.content != null
+        && card.column != null
+}
+
+exports.putCards = async (request, response) => {
+    const urlId = request.params.id;
+    const { id, title, content, column } = request.body;
+
+    if (urlId !== id)
+        return response.sendStatus(400);
+
+    const card = { title, content, column };
+
+    if (!cardDataIsValid(card))
+        return response.sendStatus(400);
+
+    const dbCard = await Card.findOne(
+        {
+            where: { id: urlId }
+        }
+    ).catch(
+        (error) => {
+            console.error('Failed to update data: ', error);
+            response.sendStatus(500);
+        }
+    );
+
+    if (!dbCard) {
+        console.error('Failed to update data: ', urlId);
+        response.sendStatus(404);
+    }
+
+    dbCard.title = title;
+    dbCard.content = content;
+    dbCard.column = column;
+
+    await dbCard.save();
+
+    return response.json(dbCard);
+}
+
+exports.deleteCards = async (request, response) => {
+    const urlId = request.params.id;
+
+    const dbCard = await Card.findOne(
+        {
+            where: { id: urlId }
+        }
+    ).catch(
+        (error) => {
+            console.error('Failed to delete data: ', error);
+            response.sendStatus(500);
+        }
+    );
+
+    if (!dbCard) {
+        console.error('Failed to delete data: ', urlId);
+        response.sendStatus(404);
+    }
+
+    await Card.destroy(
+        {
+            where: { id: urlId }
+        }
+    ).catch(
+        (error) => {
+            console.error('Failed to delete data: ', error);
+            response.sendStatus(500);
+        }
+    );
+
+    const dbCards = await Card.findAll()
+        .catch(
+            (error) => {
+                console.error('Failed to retrieve data: ', error);
+                response.sendStatus(500);
+            }
+        );
+
+    return response.json(dbCards);
+}
+
+exports.logUpdateOrDelete = async (request, response, next) => {
+    await next();
+
+    let logType = '';
+
+    switch (request.method) {
+        case 'PUT':
+            logType = 'Updated';
+            break;
+        case 'DELETE':
+            logType = 'Deleted';
+            break;
+        default:
+            return;
+    }
+
+    const now = new Date();
+    const date = now.toLocaleDateString('pt-BR');
+    const time = now.toLocaleTimeString('pt-BR');
+
+    const urlId = request.params.id;
+
+    const card = await Card.findOne(
+        {
+            where: { id: urlId }
+        }
+    );
+
+    if (card) {
+        console.log(
+            `${date} ${time} - Card ${urlId} - ${card.title} - ${logType}`
+        );
+    }
+}
--- BACK/src/index.js
+++ BACK/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { auth, login } = require('./authentication');
-const { getCards, postCards } = require('./cards');
+const { getCards, postCards, putCards, deleteCards, logUpdateOrDelete } = require('./cards');
 
 
 const app = express();
@@ -9,10 +9,13 @@ app.use(express.json());
 app.get('/', (request, response) => response.json({status: 'online'}));
 
 app.use(auth);
+app.use(logUpdateOrDelete);
 
 app.post('/login', login);
 
 app.get('/cards', getCards);
 app.post('/cards', postCards);
+app.put('/cards/:id', putCards);
+app.delete('/cards/:id', deleteCards);
 
 app.listen(5000, () => console.log('Access http://localhost:5000'));
