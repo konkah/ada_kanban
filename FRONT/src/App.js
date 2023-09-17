1928fb5daa9994bc4c4f6773da0d1738fe3a61ed
Karlos Helton Braga <karlos.hbraga@gmail.com>
2023-09-18 00:42:44 +0100

    Refactor css background and color project

--- FRONT/src/App.js
+++ FRONT/src/App.js
@@ -5,15 +5,9 @@ import './App.css';
 import Board from './components/board'
 
 
-function colors() {
-	document.body.style.background = '#282c34';
-	document.body.style.color = '#FFFFFF';
-}
-
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-		colors();
 	}
 
 	render() {
